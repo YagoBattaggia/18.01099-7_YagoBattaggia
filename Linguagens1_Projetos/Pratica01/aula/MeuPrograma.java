bd8fa598b6d94eff8a76f6ef26c69fdaa1820a2c
YagoBattaggia <yagobattaggia@uol.com.br>
2020-03-02 11:26:59 -0300

    Arquivo do meu primeiro programa

---
+++ Linguagens1_Projetos/Pratica01/aula/MeuPrograma.java
@@ -0,0 +1,7 @@
+class MeuPrograma{
+    public static void main(String[] args){
+        System.out.println("Minha primeira aplicação java!!");
+        System.out.println("Olá mundo!");
+        System.out.println("Estou programando no modo Conan. :)");
+    }
+}
