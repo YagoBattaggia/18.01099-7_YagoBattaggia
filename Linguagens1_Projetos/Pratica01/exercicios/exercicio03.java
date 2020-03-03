87e0db4b046dcbda3bcdbbabf09084f3a582ca02
YagoBattaggia <yagobattaggia@uol.com.br>
2020-03-03 15:16:44 -0300

    Exercício 03

---
+++ Linguagens1_Projetos/Pratica01/exercicios/exercicio03.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Exercicio03{
+      public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String usuario1, usuario2, senha1 ,senha2;
+        senha2 = "";
+        usuario2 = "";
+        System.out.print("Informe o usuario1: ");
+        usuario1 = scanner.nextLine();
+        System.out.print("Informe a senha1: ");
+        senha1 = scanner.nextLine();
+        while((senha1 != senha2) && (usuario1 != usuario2)){
+            System.out.print("Informe o usuario2: ");
+            usuario2 = scanner.nextLine();
+            System.out.print("Informe a senha2: ");
+            senha2 = scanner.nextLine();
+        }
+        System.out.print("ACERTOU!!");
+    } 
+}
