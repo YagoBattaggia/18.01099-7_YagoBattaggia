98a2ed03b3a0d39766c57bb4fd97195e44ff896a
YagoBattaggia <yagobattaggia@uol.com.br>
2020-06-08 12:37:08 -0300

    Feat: Main.

---
+++ Linguagens1_Projetos/AtividadeT2/AtividadeT2/src/atividade/Main.java
@@ -0,0 +1,8 @@
+package atividade;
+import atividade.sistema.*;
+public class Main{
+    public static void main(String[] args) {
+        Sistema sistema = new Sistema();
+        sistema.run();
+    }
+}
