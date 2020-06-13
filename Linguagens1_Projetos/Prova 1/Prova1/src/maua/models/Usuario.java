5824d8f52a9891451ca6bc7868c9540d644d0032
YagoBattaggia <yagobattaggia@uol.com.br>
2020-06-12 21:25:59 -0300

    Feat: Usuário

---
+++ Linguagens1_Projetos/Prova 1/Prova1/src/maua/models/Usuario.java
@@ -0,0 +1,36 @@
+package maua.models;
+
+import javax.lang.model.element.Element;
+
+/**
+ * Essa classe gerencia e modela o usuário do sistema
+ * 
+ * @author Yago Garcia Battaggia - 18.01099-7@maua.br
+ */
+public class Usuario {
+    private String nome;
+    private String email;
+    private String senha;
+
+    public Usuario(String nome, String email, String senha){
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public String getNome() {
+        return nome;
+    }
+
+    public boolean checaSenha(String tentativa){
+        if(senha.equals(tentativa){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
