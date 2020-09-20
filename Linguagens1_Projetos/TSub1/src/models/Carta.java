4855679bef8979556d62313396e7106854f73855
Yago Battaggia <38015402+YagoBattaggia@users.noreply.github.com>
2020-09-20 17:02:08 -0300

    Projeto criado; Classe Carta iniciada; Database criado

---
+++ Linguagens1_Projetos/TSub1/src/models/Carta.java
@@ -0,0 +1,19 @@
+package models;
+
+public class Carta {
+    private String url;
+    private String id;
+    private String nome;
+    private String raridade;
+    private String serie;
+    private String colecao;
+
+    public void Carta(String url, String id, String nome, String raridade, String serie, String colecao){
+        this.url = url;
+        this.id = id;
+        this.nome = nome;
+        this.raridade = raridade;
+        this.serie = serie;
+        this.colecao = colecao;
+    }
+}
